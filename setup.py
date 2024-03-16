87823bc893eb0e55d72e95987574e7e6d2e1fc4b
Roberts Slisans <roberts.slisans@gmail.com>
2024-03-16 18:24:59 +0000

    fix file packaging

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        "": ["*.json", "*.npz", "*.txt", "*.wav", "*.ogg"],
+    },
     description='Multilingual Text-to-Speech Synthesis and Voice Cloning',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
